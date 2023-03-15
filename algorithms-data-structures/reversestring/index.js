69ae196782b6f412325456ef9d34c996b3fbd87b
Randi Mays <randimays1024@gmail.com>
2023-03-15 10:36:38 -0500

    Adding Algorithms & Data Structures practice

---
+++ algorithms-data-structures/reversestring/index.js
@@ -0,0 +1,30 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// Solution 3
+const reverse = string => {
+    return string.split('').reduce((reversed, character) => character + reversed, '');
+};
+
+module.exports = reverse;
+
+// Solution 1
+// const reverse = string => {
+//     return string.split('').reverse().join('');
+// };
+
+// Solution 2
+// const reverse = string => {
+//     let reversed = '';
+
+//     for (let character of string) {
+//         reversed = character + reversed;
+//     }
+
+//     return reversed;
+// };
