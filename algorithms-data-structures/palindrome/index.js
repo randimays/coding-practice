17303a819d6ce32f2ac66ca931bd8c11e84cd122
Randi Mays <randimays1024@gmail.com>
2023-03-15 10:54:08 -0500

    Adding palindrome exercise

---
+++ algorithms-data-structures/palindrome/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+// Solution 2
+const palindrome = string => {
+    return string.split('').every((char, i) => {
+        return char === string[string.length - i - 1];
+    });
+};
+
+module.exports = palindrome;
+
+// Solution 1
+// const palindrome = string => {
+//     const reversed = string.split('').reverse().join('');
+
+//     return reversed === string;
+// };
