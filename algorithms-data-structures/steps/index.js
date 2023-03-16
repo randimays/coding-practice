0a0aad416edbfb83eb243919ace14a80382f7de2
Randi Mays <randimays1024@gmail.com>
2023-03-16 11:25:21 -0500

    Add steps exercise

---
+++ algorithms-data-structures/steps/index.js
@@ -0,0 +1,64 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+const steps = (n, row = 0, stair = '') => {
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+
+    steps(n, row, stair);
+};
+
+module.exports = steps;
+
+// Solution 1
+// const steps = n => {
+//     for (let i = 1; i <= n; i++) {
+//         console.log(('#'.repeat(i)).padEnd(n, ' '))
+//     }
+// };
+
+
+// Solution 2
+// const steps = n => {
+//     for (let row = 0; row < n; row++) {
+//         let stair = '';
+
+//         for (let column = 0; column < n; column++) {
+//             if (column <= row) {
+//                 stair += '#';
+//             } else {
+//                 stair += ' ';
+//             }
+//         }
+
+//         console.log(stair);
+//     }
+// };
