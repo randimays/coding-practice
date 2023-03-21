163f2e544e28ee8f471baffbb2c226cbcb02e2cf
Randi Mays <randimays1024@gmail.com>
2023-03-21 11:48:18 -0500

    Add fromlast exercise

---
+++ algorithms-data-structures/fromlast/index.js
@@ -0,0 +1,44 @@
+// --- Directions
+// Given a linked list, return the element n spaces
+// from the last node in the list.  Do not call the 'size'
+// method of the linked list.  Assume that n will always
+// be less than the length of the list.
+// --- Examples
+//    const list = new List();
+//    list.insertLast('a');
+//    list.insertLast('b');
+//    list.insertLast('c');
+//    list.insertLast('d');
+//    fromLast(list, 2).data // 'b'
+
+const fromLast = (list, n) => {
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    while (n > 0) {
+        fast = fast.next;
+        n--;
+    }
+
+    while (fast.next) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    return slow;
+};
+
+module.exports = fromLast;
+
+// My solution
+// const fromLast = (list, n) => {
+//     let counter = 0;
+//     let pointer = list.getFirst();
+
+//     while (pointer.next) {
+//         counter++;
+//         pointer = pointer.next;
+//     }
+
+//     return list.getAt(counter - n);
+// };
